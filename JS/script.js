9eee99e156cc428071a61bee6af1e6591bd97bfa
andreew75 <anline75@gmail.com>
2025-07-14 22:32:17 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,38 @@
-let price = prompt("Введите стоимость покупки: ");
-if (price > 500 && price <= 1000) {
-    alert("Стоимось покупки без скидки: " + price + " руб." + "\n\n" + "Скидка 3%!" + "\n" + "Сумма к оплате: " + price * 0.97 + " руб.");
-} else if (price > 1000) {
-    alert("Стоимось покупки без скидки: " + price + " руб." + "\n\n" + "Скидка 5%!" + "\n" + "Сумма к оплате: " + price * 0.95 + " руб.");
-} else {
-    alert("Скидок нет!" + "\n\n" + "Сумма к оплате: " + price + " руб.");
+document.write("<table border='1' cellspacing='0'>");
+for (let i = 1; i < 11; i++) {
+    document.write("<tr align='center'>");
+    for (let j = 1; j < 11; j++) {
+        if ((i + j) % 2 == 0) {
+            document.write("<td bgcolor='yellow'>" + i*j + "</td>");
+        } else {
+            document.write("<td bgcolor='red'>" + i*j + "</td>");
+        }
+    }
+    document.write("</tr>");
 }
+document.write("</table>");
+
+document.write("<br>");
+
+// вариант 2
+document.write("<table border='1' cellspacing='0'>");
+
+document.write("<tr><td></td>");
+for (let j = 1; j < 11; j++) {
+    document.write("<td align='center'>" + j + "</td>");
+}
+document.write("</tr>");
+
+for (let i = 1; i < 11; i++) {
+    document.write("<tr>");
+    document.write("<td align='center'>" + i + "</td>");
+    for (let j = 1; j < 11; j++) {
+        if ((i + j) % 2 == 0) {
+            document.write("<td bgcolor='yellow' align='center'>" + i*j + "</td>");
+        } else {
+            document.write("<td bgcolor='red' align='center'>" + i*j + "</td>");
+        }
+    }
+    document.write("</tr>");
+}
+document.write("</table>");

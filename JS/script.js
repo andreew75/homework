16d5b3ceb8a5c9a65cba805502bc689f2fd8ab14
andreew75 <anline75@gmail.com>
2025-07-23 23:02:48 +0300

    update

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,9 @@
 
-mouths = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-for (let i = 0; i < mouths.length; i++) {
-        document.writeln(`<div class='block'>${mouths[i]}</div>`);
-}
+let img = document.getElementById("image");
 
-let div = document.querySelectorAll('.block');
-
-let divColor = () => {
-    for (let i = 0; i < div.length; i++) {
-        div[i].style.background = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
-    }
+function hide() {
+    img.style.display = "none";
 }
-divColor();
+function show() {
+    img.style.display = "block";
+}

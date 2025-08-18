7a4a59a61eb9fee4665873261d7dca4be290b6a6
andreew75 <anline75@gmail.com>
2025-08-18 23:24:28 +0300

    Update homework

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,28 @@
+function swapImages() {
+    const firstImgNum = parseInt(document.getElementById("choose_1").value);
+    const secondImgNum = parseInt(document.getElementById("choose_2").value);
 
-let img = document.getElementById("image");
+    if (firstImgNum < 1 || firstImgNum > 3 || secondImgNum < 1 || secondImgNum > 3) {
+        alert("Введите числа от 1 до 3!");
+        return;
+    }
 
-function hide() {
-    img.style.display = "none";
-}
-function show() {
-    img.style.display = "block";
+    if (firstImgNum === secondImgNum) {
+        alert("Выбраны одинаковые изображения!");
+        return;
+    }
+
+    const firstImg = document.getElementById(`image${firstImgNum}`);
+    const secondImg = document.getElementById(`image${secondImgNum}`);
+
+    let tempSrc = firstImg.src;
+    firstImg.src = secondImg.src;
+    secondImg.src = tempSrc;
+
+    firstImg.style.transform = "scale(0.98)";
+    secondImg.style.transform = "scale(0.98)";
+    setTimeout(() => {
+        firstImg.style.transform = "scale(1)";
+        secondImg.style.transform = "scale(1)";
+    }, 150);
 }

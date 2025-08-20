49056094bb390887c2b02a48fdf8b0652b42fdf7
andreew75 <anline75@gmail.com>
2025-08-20 23:10:31 +0300

    Update

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,11 @@
-function swapImages() {
-    const firstImgNum = parseInt(document.getElementById("choose_1").value);
-    const secondImgNum = parseInt(document.getElementById("choose_2").value);
+let removeButtons = document.querySelectorAll('.remove-button');
 
-    if (firstImgNum < 1 || firstImgNum > 3 || secondImgNum < 1 || secondImgNum > 3) {
-        alert("Введите числа от 1 до 3!");
-        return;
-    }
-
-    if (firstImgNum === secondImgNum) {
-        alert("Выбраны одинаковые изображения!");
-        return;
-    }
-
-    const firstImg = document.getElementById(`image${firstImgNum}`);
-    const secondImg = document.getElementById(`image${secondImgNum}`);
-
-    let tempSrc = firstImg.src;
-    firstImg.src = secondImg.src;
-    secondImg.src = tempSrc;
-
-    firstImg.style.transform = "scale(0.98)";
-    secondImg.style.transform = "scale(0.98)";
-    setTimeout(() => {
-        firstImg.style.transform = "scale(1)";
-        secondImg.style.transform = "scale(1)";
-    }, 150);
+for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', function() {
+        let pane = removeButtons[i].parentNode;
+        pane.remove();
+    });
 }
+
+
+

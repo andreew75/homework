a65f75aa2bbf45ad555093d9afac9ce78c645bc2
andreew75 <anline75@gmail.com>
2025-08-28 00:09:21 +0300

    Update

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,15 @@
-let choose = document.querySelector("#btn")
-choose.addEventListener("click", function () {
-    let inputCheck = document.querySelector("input[type='radio']:checked");
-    if (inputCheck) {
-        alert(inputCheck.value);
-    } else {
-        alert("Пожалуйста, выберите загадку!");
-    }
-});
+function processMessage() {
+    let textArea = document.querySelector("#guest-book").value;
+    let userName = document.querySelector("#your-name").value;
 
+    const patternEmail = /([0-9a-z_.-]+@[0-9a-z_^\.]+\.[a-z]{2,3})/ig;
+    let formatText= textArea.replace(patternEmail, "<span style='color:red'>$1</span>");
 
-//let choose = document.querySelector("#btn")
-//choose.addEventListener("click", function () {
-//    let inputCheck = document.forms["form-choose"].radio;
-//    for (let i = 0; i < inputCheck.length; i++) {
-//        if (inputCheck[i].checked) {
-//            alert(inputCheck[i].value);
-//            return;
-//        }
-//    }
-//    alert("Пожалуйста, выберите загадку!");
-//});
-
-
+    let message = document.querySelector("#message");
+    message.innerHTML = `
+        <fieldset>
+            <legend>Сообщение от пользователя <b>${userName}</b></legend>
+            <p><b>Текст сообщения:</b><br>${formatText}</p>
+        </fieldset>
+    `;
+};

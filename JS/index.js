25829baef0cb512edb98a496d2db583b9bd960b0
andreew75 <anline75@gmail.com>
2025-09-15 22:45:12 +0300

    Update

--- JS/index.js
+++ JS/index.js
@@ -1,42 +1,18 @@
+document.querySelector("#load").addEventListener("click", loadUsers);
 
-let form = document.form1;
-let modalWindow = document.getElementById('modalWindow');
-
-function showModal() {
-    modalWindow.classList.add("active");
-    document.body.style.overflow = 'hidden';
-}
-
-function closeModal() {
-    modalWindow.classList.remove("active");
-    document.body.style.overflow = 'auto';
-}
-modalWindow.addEventListener('click', function (event) {
-    if (event.target === modalWindow) {
-        closeModal();
+async function loadUsers() {
+    try {
+        const url = 'https://jsonplaceholder.typicode.com/todos';
+        let response = await fetch(url);
+        let js = await response.json();
+        let ul = document.querySelector("#list");
+        let html = js.map(function (item) {
+            if (item.completed === true) {
+                return `<li>Пользователь ${item.userId} выполнил задачу № ${item.id} (${item.title})</li>`;
+            }
+        })
+        ul.insertAdjacentHTML("afterbegin", html.join(" "));
+    } catch (error) {
+        alert("Ошибка загрузки данных");
     }
-});
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    let request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-
-    let formData = new FormData(form);
-    request.send(formData);
-
-    request.addEventListener("load",  () => {
-        if (request.status === 200) {
-            console.log(request.response);
-            showModal();
-            window.modalTimeout = setTimeout(closeModal, 5000);
-        } else {
-            alert('Произошла ошибка при отправке.');
-        }
-        form.reset();
-    })
-})
-
-
-
+}
